cd295f025c9ad9a698fb0868c7fb6526c2fbb1a7
PhilippEdrichCodes <philipp@edrich.codes>
2022-06-08 23:09:31 +0200

    total refactoring
    GenreDialog.js fertig
    
    Signed-off-by: PhilippEdrichCodes <philipp@edrich.codes>

--- src/components/GenreDialog.js
+++ src/components/GenreDialog.js
@@ -32,7 +32,7 @@ class GenreDialog extends React.Component {
    * Es wird eine Bestätigung mittels PopUp angefordert.
    * @param {String} name - der Name des zu löschenden Genre
    */
-  gruppeEntfernen (name) {
+  genreLoeschen (name) {
     if (window.confirm("Wollen Sie diesen Eintrag wirklich löschen?!")) {
       Modell.genreLoeschen(name)
     }
@@ -41,54 +41,56 @@ class GenreDialog extends React.Component {
 
   render () {
     const genreListe = []
-    for (let gruppe of this.state.genreListe) {
-      genreListe.push(
-        <GruppeBearbeitenTag
-          key={gruppe.id}
-          genre={gruppe}
-          entfernenHandler={() => this.gruppeEntfernen(gruppe.name)}/>
-      )
+    for (let genre of this.state.genreListe) {
+      genreListe.push(<GruppeBearbeitenTag
+        key={genre.id}
+        genre={genre}
+        entfernenHandler={() => this.genreLoeschen(genre.name)}/>)
     }
 
-    return (
-      <div className="mdc-dialog mdc-dialog--open popup">
-        <div className="mdc-dialog__container">
-          <div className="mdc-dialog__surface"
-               role="alertdialog"
-               aria-modal="true"
-               aria-labelledby="my-dialog-title"
-               aria-describedby="my-dialog-content">
-            <h2 className="mdc-dialog__title" id="my-dialog-title">Genres bearbeiten</h2>
+    return (<div className="mdc-dialog mdc-dialog--open popup">
+      <div className="mdc-dialog__container">
+        <div className="mdc-dialog__surface"
+             role="alertdialog"
+             aria-modal="true"
+             aria-labelledby="my-dialog-title"
+             aria-describedby="my-dialog-content">
+          <h2 className="mdc-dialog__title" id="my-dialog-title">Genres bearbeiten</h2>
 
-            <div className="mdc-dialog__content" id="my-dialog-content">
-              <label
-                className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
-                <span className="mdc-text-field__ripple"></span>
-                <input className="mdc-text-field__input" type="search" id="eingabe"
-                       placeholder="Genre hinzufügen" autoComplete="false"
-                       onKeyDown={e => (e.key === "Enter") ? this.genreHinzufuegen() : ""}/>
-                <span className="mdc-line-ripple"></span>
-                <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
-                   tabIndex="0" role="button"
-                   onClick={() => this.genreHinzufuegen()}>add_circle</i>
-              </label>
+          <div className="mdc-dialog__content"
+               id="my-dialog-content">
+            <label
+              className="mdc-text-field mdc-text-field--filled mdc-text-field--with-trailing-icon mdc-text-field--no-label">
+              <span className="mdc-text-field__ripple"></span>
+              <input className="mdc-text-field__input"
+                     type="search"
+                     id="eingabe"
+                     placeholder="Genre hinzufügen"
+                     autoComplete="false"
+                     onKeyDown={e => (e.key === "Enter") ? this.genreHinzufuegen() : ""}/>
+              <span className="mdc-line-ripple"></span>
+              <i className="material-icons mdc-text-field__icon mdc-text-field__icon--trailing"
+                 tabIndex="0"
+                 role="button"
+                 onClick={() => this.genreHinzufuegen()}>add_circle</i>
+            </label>
 
-              <dl className="mdc-deprecated-list">
-                {genreListe}
-              </dl>
-            </div>
-            <div className="mdc-dialog__actions">
-              <button type="button" className="mdc-button mdc-dialog__button"
-                      onClick={this.props.onDialogClose}>
-                <div className="mdc-button__ripple"></div>
-                <span className="mdc-button__label">Schließen</span>
-              </button>
-            </div>
+            <dl className="mdc-deprecated-list">
+              {genreListe}
+            </dl>
+          </div>
+          <div className="mdc-dialog__actions">
+            <button type="button"
+                    className="mdc-button mdc-dialog__button"
+                    onClick={this.props.onDialogClose}>
+              <div className="mdc-button__ripple"></div>
+              <span className="mdc-button__label">Schließen</span>
+            </button>
           </div>
         </div>
-        <div className="mdc-dialog__scrim"></div>
       </div>
-    )
+      <div className="mdc-dialog__scrim"></div>
+    </div>)
   }
 }
 
